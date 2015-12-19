f37b2eb3fe237e056372337519d1187b45679342
tibalt <gtibalt@gmail.com>
2015-12-19 20:54:12 +0100

    import now skips movies without imdb id

--- app/import/index.js
+++ app/import/index.js
@@ -11,22 +11,26 @@ const checkIfMovieExists = (imdbId) => {
 };
 
 const createMovieIfNotExists = (movie) => {
-    checkIfMovieExists(movie.imdbId)
-        .then(doesExist => {
-            if (doesExist) {
-                console.log('Movie exists: [' + movie.imdbId + '] ' + movie.title)
-            } else {
-                fetchDetails(movie.imdbId)
-                    .then(movieDetails =>
-                        createMovie(_.merge({}, movie, movieDetails)))
-                    .catch(ex => console.error('Error fetching', ex))
-            }
-        })
-        .catch(err => console.error(err))
+    if (!movie.imdbId) {
+        console.log('Movie has no IMDB id, skipping:', movie.title)
+    } else {
+        checkIfMovieExists(movie.imdbId)
+            .then(doesExist => {
+                if (doesExist) {
+                    console.log('Movie exists, skipping: [' + movie.imdbId + '] ' + movie.title)
+                } else {
+                    fetchDetails(movie.imdbId)
+                        .then(movieDetails =>
+                            createMovie(_.merge({}, movie, movieDetails)))
+                        .catch(ex => console.error('Error fetching', ex))
+                }
+            })
+            .catch(err => console.error(err))
+    }
 };
 
 const createMovie = (data) => {
-    (new Movie(data))
+    return (new Movie(data))
         .save()
         .then((savedMovie) => console.log('Saved movie', savedMovie.title))
         .catch(ex => console.error('Error saving movie:', ex));
--- app/import/providers.js
+++ app/import/providers.js
@@ -1,19 +1,19 @@
 module.exports = {
     rt: {
         buildUri: (key) => 'http://api.rottentomatoes.com/api/public/v1.0/lists/dvds/new_releases.json?' +
-            'country=us&page_limit=5&apikey=' + key,
+            'country=us&page_limit=20&apikey=' + key,
         moviesProp: 'movies',
         converters: {
             title: ({ title }) => title,
             year: ({ year }) => year,
-            imdbId: ({ alternate_ids }) => 'tt'+alternate_ids.imdb,
+            imdbId: ({ alternate_ids }) => alternate_ids ? 'tt'+alternate_ids.imdb : null,
             poster: ({ posters }) => ({ small: posters.thumbnail }),
             desc: ({ synopsis }) => synopsis,
             runtime: ({ runtime }) => runtime,
         },
     },
     tmdb: {
-        uri: 'http://api.themoviedb.org/3/discover/movie?' + 'api_key=' + keys.tmdb,
+        buildUri: (key) => 'http://api.themoviedb.org/3/discover/movie?' + 'api_key=' + key,
         moviesProp: 'results',
     },
     myapi: {
@@ -21,7 +21,7 @@ module.exports = {
             'idIMDB=' + imdbId + '&token=' + key + '&format=json&language=en-us&actors=1',
         movieProp: 'data.movies.0',
         converters: {
-            votes: ({ votes }) => Number(votes.replace(',', '')),
+            votes: ({ votes }) => votes ? Number(votes.replace(',', '')) : 0,
             rating: ({ rating }) => Number(rating),
             poster: ({ urlPoster }) => ({ normal: urlPoster }),
             genres: ({ genres }) => genres,
