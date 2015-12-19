b145b6b788bd67e80dde941859215a97e5569914
tibalt <gtibalt@gmail.com>
2015-12-19 20:24:32 +0100

    movie schema added

---
+++ app/models/Movie.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema({
+    imdbId: { type: String, unique: true },
+    title: String,
+    rating: Number,
+    votes: Number,
+    year: Number,
+    desc: String,
+    poster: {
+        small: String,
+        normal: String,
+        large: String,
+    },
+    runtime: Number,
+    genres: [String],
+    directors: [{
+        name: String,
+        imdbId: String,
+    }],
+    actors: [{
+        name: String,
+        imdbId: String,
+        character: String,
+        photo: String,
+    }],
+    updated_at: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model('Movie', Schema);
