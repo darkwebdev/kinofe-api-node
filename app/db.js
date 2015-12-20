5897a02efd5c8ac54db06dfaf2ab259b04a8f1e9
tibalt <gtibalt@gmail.com>
2015-12-20 20:57:46 +0100

    di for db added

--- app/db.js
+++ app/db.js
@@ -1,14 +1,13 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-const db = require('./config/main.json').db;
+module.exports = (db) =>
+    new Promise((resolve, reject) => {
+        mongoose.connect(db);
 
-mongoose.connect(db);
-
-module.exports = new Promise((resolve, reject) =>
-    mongoose.connection
-        .on('error', (err) =>
-            reject(console.error('DB connection error:', err.message)))
-        .once('open', () =>
-            resolve(console.log(db + ' connected')))
-);
+        return mongoose.connection
+            .on('error', (err) =>
+                reject(console.error('DB connection error:', err.message)))
+            .once('open', () =>
+                resolve(console.log(db + ' connected')))
+    });
