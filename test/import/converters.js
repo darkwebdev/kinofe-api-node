f8c580ddcaecacf22445f1090f2f6b23dc32ff38
tibalt <gtibalt@gmail.com>
2015-12-26 22:22:35 +0100

    tests for converters improved, added edge cases

--- test/import/converters.js
+++ test/import/converters.js
@@ -4,6 +4,9 @@ const { convertListToSchema, convertDetailsToSchema } = require('../../app/impor
 const { rt, myapi } = require('../../app/import/providers');
 const newReleases = require('./data/newReleases');
 const movieDetails = require('./data/movieDetails');
+const movieDetailsNoDirectors = require('./data/movieDetailsNoDirectors');
+const movieDetailsNoActors = require('./data/movieDetailsNoActors');
+const movieDetailsNoGenres = require('./data/movieDetailsNoGenres');
 const convertedReleases = require('./data/convertedReleases');
 const convertedDetails = require('./data/convertedDetails');
 
@@ -16,5 +19,23 @@ describe('Converters', () => {
     });
     it('should create proper data structures from Movie Details JSON', () => {
         expect(convertDetailsToSchema(movieDetails, detailsProvider)).to.deep.equal(convertedDetails);
+    });
+
+    context('on JSON without directors', () => {
+        it('should throw an exeption', () => {
+            expect(() => convertDetailsToSchema(movieDetailsNoDirectors, detailsProvider)).to.throw(TypeError);
+        });
+    });
+
+    context('on JSON without actors', () => {
+        it('should throw an exeption', () => {
+            expect(() => convertDetailsToSchema(movieDetailsNoActors, detailsProvider)).to.throw(TypeError);
+        });
+    });
+
+    context('on JSON without genres', () => {
+        it('should throw an exeption', () => {
+            expect(() => convertDetailsToSchema(movieDetailsNoGenres, detailsProvider)).to.throw(TypeError);
+        });
     });
 });
